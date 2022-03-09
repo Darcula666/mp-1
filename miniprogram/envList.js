6ca1c99c643c79b904c61a833e34540ed450580d
tiankun <18787176584@163.com>
2022-03-09 14:50:54 +0800

    Initial Commit

---
+++ miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"dev-ivspr","alias":"dev"},{"envId":"pro-oiop5","alias":"pro"}]
+const isMac = true
+module.exports = {
+    envList,
+    isMac
+}
---
+++ miniprogram/pages/list/index.js
@@ -0,0 +1,92 @@
+// pages/list/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  info:'手机号支持长按选中效果',
+  count:0,
+  type:1
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  console.log('上啦触底了')
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  btnTapHandel(e) {
+    let cc=wx.getDeviceInfo();
+    console.log(cc);
+    this.setData({
+      count:cc.platform
+
+    })
+  },
+  takePhoto() {
+    const ctx = wx.createCameraContext()
+    ctx.takePhoto({
+      quality: 'high',
+      success: (res) => {
+        this.setData({
+          src: res.tempImagePath
+        })
+      }
+    })
+  },
+  error(e) {
+    console.log(e.detail)
+  }
+  
+})
